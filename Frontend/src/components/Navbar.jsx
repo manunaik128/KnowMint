ea8554587c43e7ce356de52524096a00e95bbe7c
manunaik128 <manumn2005@gmail.com>
2026-02-26 14:50:54 +0530

    nav bar created

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "../styles/Navbar.css"
+import Logo from "../assets/logo.png"
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="nav">
+        <div className="logo">
+          <img src={Logo} />
+        </div>
+
+        <div className="options">
+          <ul>
+            <li>Explore</li>
+            <li>My Notes</li>
+            <li>Upload</li>
+          </ul>
+
+          <button id='login-btn'>Login</button>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
