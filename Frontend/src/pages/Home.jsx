7ea89dd9a0b99ff7dfd8ef5e2181cc39c967d65f
manunaik128 <manumn2005@gmail.com>
2026-02-27 20:43:42 +0530

    created body page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,62 +1,88 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import "../styles/Home.css";
-import Background from "../assets/back.png";
-import Notes from "../assets/notes.png";
-import Upload from "../assets/upload.png"
-import Explore from "../assets/explore.png"
-import File from "../assets/file.png"
+import Access from "../assets/access.png";
+import Hand from "../assets/hand.png";
+import Tick from "../assets/tick.png";
+import Search from "../assets/search.png";
+import Upload from "../assets/upload.png";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <div className="home">
-        <div className="text">
-          <div className="welcome">
-            <img src={Notes} alt="" />
-            <h1>Welcome to KnowMint</h1>
+        <div className="file-details">
+          <div className="details">
+            <div className="texts">
+              <span id="main-txt">
+                Study master with <br />
+                shared notes. <br />
+              </span>
+
+              <br />
+              <p id="paragraphs">
+                Upload PDFs, organize them by semester and subject, 
+                and quickly <br />find reliable study materials. Collaborate 
+                with classmates and keep all <br />your notes in one place.
+              </p>
+            </div>
+            <div className="btns">
+              <button id="upload">Upload Notes</button>
+              <button id="upload">Explore Liabrary</button>
+            </div>
           </div>
 
-          <div className="quote">
-            <p id="details">
-              Empower your learning by sharing and accessing quality notes.
-              Discover, upload, and organize notes across all semesters. Access
-              reliable study materials anytime, anywhere you need. Collaborate
-              with peers and simplify your academic journey. Upload PDFs easily
-              and reach students across branches. One platform to explore,
-              share, and grow knowledge. Find subject-wise notes faster with
-              smart search tools.
-            </p>
+          <div className="extra-details">
+            <div className="border">
+              <div className="subjects">
+                <div className="text-icon">
+                  <img src={Hand} alt="" />
+                </div>
+                <div className="icon-defination">
+                  <span id="icon-details">Subject-wise organization</span>
+                </div>
+              </div>
+
+              <div className="search">
+                <div className="text-icon">
+                  <img src={Search} alt="" />
+                </div>
+                <div className="icon-defination">
+                  <span id="icon-details">Smart search and filters</span>
+                </div>
+              </div>
+              <div className="share">
+                <div className="text-icon">
+                  <img src={Access} alt="" />
+                </div>
+                <div className="icon-defination">
+                  <span id="icon-details">Share with classmates</span>
+                </div>
+              </div>
+              <div className="access">
+                <div className="text-icon">
+                  <img src={Tick} alt="" />
+                </div>
+                <div className="icon-defination">
+                  <span id="icon-details">Access anywhere</span>
+                </div>
+              </div>
+            </div>
           </div>
+        </div>
 
-          <div className="option-btn">
-            <div className="upload-notes">
-              <div className="upload">
-                <img src={Upload} alt="" />
-              </div>
-              <div className="txt">
-              <span id="content-text">Upload</span>
-              </div>
-            </div>
-            <div className="my-notes">
-              <div className="notes">
-                 <img src={File} alt="" />
-              </div>
-                 <div className="txt">
-              <span id="content-text">My Notes</span>
-              </div>
-            </div>
-
-            <div className="explore-notes">
-              <div className="explore">
-               <img src={Explore} alt="" />
-               </div>
-                  <div className="txt">
-              <span id="content-text">Explore</span>
-              </div>
+        <div className="files-options">
+          <div className="upload-opt">
+            <div className="upload-pic">
+              <img src={Upload} alt="" />
 
             </div>
+            <div className="upload-text">
+              
+            </div>
+            
+
           </div>
         </div>
       </div>
